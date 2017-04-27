2f6d5d17290f1a83fb193559f8c71f8f8cee03b6
Justin Obney <justinobney@gmail.com>
2017-04-27 09:28:03 -0500

    chore: cleanup

--- src/TestComponent.spec.js
+++ src/TestComponent.spec.js
@@ -4,7 +4,7 @@ import {create} from 'react-test-renderer';
 const TestComponent = ({test}) => <div>{test}</div>;
 
 describe('Component: TestComponent', () => {
-  it('sample test', () => {
+  xit('sample test', () => {
     const tree = create(<TestComponent test="foo" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
