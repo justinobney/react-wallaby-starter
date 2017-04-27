3e98ee4c231a937fab1a41ad4413d5c2d3c60a74
Justin Obney <justinobney@gmail.com>
2017-04-27 10:08:36 -0500

    feat(styles): testing the waters with glamorous.

--- src/layout/AppFooter.js
+++ src/layout/AppFooter.js
@@ -1,33 +1,32 @@
 /** @jsx createElement */
 import {createElement} from 'react';
-import './footer.scss';
+import glamorous from 'glamorous';
+import {Anchor} from 'styles';
+
+const Footer = glamorous.footer({
+  textAlign: 'center',
+  paddingBottom: 10,
+});
+
+const SupportBar = glamorous.p({
+  marginBottom: 10,
+});
+
+const Copyright = glamorous.p({});
 
 const AppFooter = () => (
-  <div id="footer-container">
-    <footer id="site-footer">
-      <div id="support-bar">
-        <p className="text-center" id="call">
-          Call <a href="tel:1-800-551-8900">1-800-551-8900</a> for support.
-        </p>
-      </div>
-      <div className="ui container">
-        <div id="footer-content">
-          <p className="text-center">
-            <a href="http://justinobney.com">
-              {/* <img
-                id="company-logo"
-                src={require('../img/logo-print.png')}
-                alt={COMPANY_NAME}
-              /> */}
-            </a>
-          </p>
-          <p className="text-center">
-            {`© Elevator2, LLC ${new Date().getFullYear()}. All Rights Reserved.`}
-          </p>
-        </div>
-      </div>
-    </footer>
-  </div>
+  <Footer>
+    <SupportBar>
+      Call
+      {' '}
+      <Anchor href="tel:1-800-551-8900">1-800-551-8900</Anchor>
+      {' '}
+      for support.
+    </SupportBar>
+    <Copyright>
+      {`© Elevator2, LLC ${new Date().getFullYear()}. All Rights Reserved.`}
+    </Copyright>
+  </Footer>
 );
 
 export default AppFooter;
---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import glamorous from 'glamorous';
+
+export const colors = {
+  brandPrimary: '#00573d',
+};
+
+// common styled components
+export const Anchor = glamorous.a({
+  color: colors.brandPrimary,
+});
