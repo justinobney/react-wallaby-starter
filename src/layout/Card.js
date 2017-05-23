9b9d96423f1c179ee41ab83c13fe22a3ae57344d
Justin Obney <justinobney@gmail.com>
2017-05-23 16:28:41 -0500

    tests: jest-glamor-react

--- src/layout/Card.js
+++ src/layout/Card.js
@@ -2,7 +2,12 @@
 import {createElement} from 'react';
 import {Container, Header} from 'semantic-ui-react';
 import glamorous from 'glamorous';
+import {matcher, serializer} from 'jest-glamor-react';
+
 import {colors, contentBase} from 'styles';
+
+expect.addSnapshotSerializer(serializer);
+expect.extend(matcher);
 
 const Wrapper = glamorous(Container)(contentBase);
 const Title = glamorous(Header)({
