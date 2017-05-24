8ca39cb56d488feeebe86c6232b2d4504e17ec5a
Justin Obney <justinobney@gmail.com>
2017-05-24 08:13:31 -0500

    chore: react-scripts 1.0.5 updates

--- stories/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {storiesOf, linkTo} from '@kadira/storybook';
-import Welcome from './Welcome';
-
-import 'semantic-ui-css/semantic.css';
-
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
