4be7fd83bbe320c82123964afee096a10497c919
Justin Obney <justinobney@gmail.com>
2017-11-06 13:11:32 -0600

    feat: wire up webpack dashboard

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,15 @@
 const rewireEmotion = require('react-app-rewire-emotion');
+var DashboardPlugin = require('webpack-dashboard/plugin');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  return rewireEmotion(config, env);
+  const modifiedConfig = rewireEmotion(config, env);
+
+  if (env === 'development') {
+    modifiedConfig.plugins = (modifiedConfig.plugins || []).concat([
+      new DashboardPlugin(),
+    ]);
+  }
+
+  return modifiedConfig;
 };
